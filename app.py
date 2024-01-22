ea42567af3d987b8001eb9df6db78bead83d576d
suryapratapverma-09 <115792303+suryapratapverma-09@users.noreply.github.com>
2024-01-22 19:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-import os
-import google.generativeai as genai 
 import streamlit as st
+import os
+from PIL import Image #python image library
+import google.generativeai as genai 
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model=genai.GenerativeModel("gemini-pro")
-chat=model.start_chat(history=[])
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
-def get_gemini_response(question):
-    response=chat.send_message(question,stram=True)
-    return response
+model= genai.GenerativeModel('gemini-pro-vision')
 
-st.set_page_config("Q&A Chatbot")
-st.header("conversational chatbot")
+def get_gemini_response(input,image_data,user_promt):
+    response = model.generate_content([input,image_data[0],user_promt])
+    return response.text
 
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history']=[]
+def input_image_details(upload-file):
+    if uploaded_file is not None:
+        bytes_data=uploaded_file.getvalue()
+        image_parts = [{
+            'mime_type':uploaded_file.type,
+            'data':bytes_data
+        }]
+        return image_parts
+    else:
+        raise FileNotFoundError('No file uploaded')
+    
+st.header('Multilanguage invoice Extractor')
 
-input=st.text_input("input: ",key="input")
-submit=st.button("send")
+input = st.text_input('Input Promt',key='input')
+uploaded_file=st.file_uploader('Image',type=['jpg','jpeg','png'])
 
-if input and submit:
-    response=get_gemini_response(input)
-    st.session_state['chat_history'].append(("you",input))
-    st.subheader("response:")
-    for chunk in response:
-        st.write(chunk.text)
-        st.response_state['chat_history'].append(("response",chunk.text))
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image,caption='uploaded File',use_column_width=True)
 
-st.subheader("chat history:")
-for role, response in st.session_state['chat_history']:
-    st.write(f"{role}:{response}")
+sub = st.button('tell me about the invoice')
+
+input_promt = """you are expert in understanding invoices.
+we will upload an image as a invoice and you will have to answer any
+questions based on the uploded invoice image."""
+
+if sub:
+    with st.spinner('wait'):
+        image_data = input_image_details(uploaded_file)
+        response=get_gemini_response(input_promt,image_data,input)
+        st.subheader('the response is')
+        st.text_area(label="",value=response,height=500)
